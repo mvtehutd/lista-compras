78934d16b02c1118a26fe9c6199e1e7c8bd8a51c
mcv <100847921+mvtehutd@users.noreply.github.com>
2023-08-24 18:50:26 -0300

    Gerando o HTML sem as tabelas ainda

--- src/main/java/br/ufscar/dc/compiladores/lc/gerador/LcGerador.java
+++ src/main/java/br/ufscar/dc/compiladores/lc/gerador/LcGerador.java
@@ -1,8 +1,12 @@
 package br.ufscar.dc.compiladores.lc.gerador;
 
+import br.ufscar.dc.compiladores.lc.gerador.TabelaDeSimbolos.TipoLc;
 import br.ufscar.dc.compiladores.lc.gerador.calculadoras.CalculadoraEconomia;
 import br.ufscar.dc.compiladores.lc.gerador.calculadoras.CalculadoraEconomiaBasica;
 import br.ufscar.dc.compiladores.parser.LcBaseVisitor;
+import br.ufscar.dc.compiladores.parser.LcParser.LojaContext;
+import br.ufscar.dc.compiladores.parser.LcParser.Produto_listaContext;
+import br.ufscar.dc.compiladores.parser.LcParser.Produto_lojaContext;
 import br.ufscar.dc.compiladores.parser.LcParser.ProgramaContext;
 
 public class LcGerador extends LcBaseVisitor<Void> {
@@ -13,8 +17,35 @@ public class LcGerador extends LcBaseVisitor<Void> {
 
     StringBuilder saida;
 
+    public LcGerador() {
+        saida = new StringBuilder();
+        this.tabela = new TabelaDeSimbolos();
+    }
+
     @Override
     public Void visitPrograma(ProgramaContext ctx) {
+        // Adiciona o Início do HTML (até chegar na tabela)
+        String nomeLista = ctx.lista().IDENT().getText();
+        saida.append("<!DOCTYPE html>\n<html lang='pt'>\n<head>\n<style>\nbody{ background-color: #5ab2fe; }\n" +
+            "table { background-color: white; font-family: Arial, Helvetica, sans-serif; border-collapse: collapse; width: 70%; }\n" +
+            "td, th { padding: 8px; text-align: center; }\n tr:nth-child(even){background-color: #f2f2f2;}\n" + 
+            "th { padding-top: 12px; padding-bottom: 12px; text-align: center; background-color: #04AA6D; color: white; }\n" + 
+            "h1 { text-align: center; color: white; text-shadow: -2px 0 black, 0 2px black, 2px 0 black, 0 -2px black; " +
+            "font-family:Verdana, Geneva, Tahoma, sans-serif;padding: 20px; }\n </style>\n<meta charset='UTF-8'>" +
+            "<meta name='viewport' content='width=device-width, initial-scale=1.0'>\n<title>" + nomeLista +
+            "</title>\n</head>\n<body>\n<h1>" + nomeLista + "</h1>\n<br>\n");
+
+        // Adiciona os produtos da loja na tabela de símbolos
+        for (LojaContext loja : ctx.loja()) {
+            visitLoja(loja);
+        }
+
+        // Adiciona os produtos da lista na tabela de símbolos
+        for (Produto_listaContext produtoLista : ctx.lista().produto_lista()) {
+            tabela.adicionar(produtoLista.IDENT().getText(), LcSemanticoUtils.verificaTipo(produtoLista.UNIDADE().getText()), null, null, produtoLista.quant.getText());
+        }
+
+        // Fazer o cálculo para montar a lista desejada nas lojas e suas tabelas HTML
         /////////////
         CalculadoraEconomia calculoEcnomia = null;
         switch (propriedadeLista) {
@@ -28,7 +59,22 @@ public class LcGerador extends LcBaseVisitor<Void> {
 
         saida.append(calculoEcnomia.retornaListaDeCompras(tabela));
         ///////////
+
+        // Adiciona o restante do HTML
+        saida.append("</body>\n</html>");
         return null;
     }
     
+    @Override
+    public Void visitLoja(LojaContext ctx) {
+        String nomeLoja = ctx.IDENT().getText();
+        TabelaDeSimbolos tabelaProdutos = new TabelaDeSimbolos();
+        for (Produto_lojaContext produto : ctx.produto_loja()) {
+            tabelaProdutos.adicionar(produto.IDENT().getText(),
+                    LcSemanticoUtils.verificaTipo(produto.UNIDADE().getText()), null,
+                    produto.valor.getText(), produto.quant.getText());
+        }
+        tabela.adicionar(nomeLoja, TipoLc.LOJA, tabelaProdutos, null, null);
+        return null;
+    }
 }
